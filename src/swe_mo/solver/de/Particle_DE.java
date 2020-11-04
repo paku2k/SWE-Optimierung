7f71d9a97ba197080d6a39d65914e88fcf5cf88c
DannyBaetler <baetler@gmail.com>
2020-11-04 14:26:00 +0100

    Added static particle Attribut a.

---
+++ src/swe_mo/solver/de/Particle_DE.java
@@ -0,0 +1,7 @@
+package swe_mo.solver.de;
+
+public class Particle_DE {
+
+	static int a = 0;
+	
+}
---
+++ src/swe_mo/solver/de/Test_partikel.java
@@ -0,0 +1,13 @@
+package swe_mo.solver.de;
+
+
+public class Test_partikel {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Der Partikel hat das Attribut a mit dem Wert: "+ Particle_DE.a);
+		
+	}
+
+}
