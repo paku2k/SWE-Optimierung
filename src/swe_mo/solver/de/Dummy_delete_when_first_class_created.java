c517525f56e30404fe6333510b558fd7b80312e9
DannyBaetler <baetler@gmail.com>
2020-11-04 12:58:04 +0100

    Kommentar geändert und hinzugefügt

--- src/swe_mo/solver/de/Dummy_delete_when_first_class_created.java
+++ src/swe_mo/solver/de/Dummy_delete_when_first_class_created.java
@@ -1,6 +1,9 @@
 package swe_mo.solver.de;
 
 public abstract class Dummy_delete_when_first_class_created {
-	//Dies ist ein Kommentar von Simon
-
+	//Dies ist ein Kommentar von Daniel
+	//Simon stinkt
+	//David ist cool
 }
+
+
