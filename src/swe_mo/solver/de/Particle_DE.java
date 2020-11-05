709fbfe4c4604e164e9d22632a180445762519a2
DannyBaetler <baetler@gmail.com>
2020-11-05 14:57:26 +0100

    add CRN - modify Particle_DE
    Create Random Number Class (CRN) has been added. Particle_DE Class has
    been modified.

--- src/swe_mo/solver/de/Particle_DE.java
+++ src/swe_mo/solver/de/Particle_DE.java
@@ -1,7 +1,16 @@
 package swe_mo.solver.de;
+import java.util.ArrayList;
 
 public class Particle_DE {
 
 	static int a = 0;
+	ArrayList<Double> position = new ArrayList<Double>(); 
+	
+	
+	public Particle_DE(int dimension, int max, int min) {
+	    for(int i = dimension; i>0; i--) {
+	    	position.add(CRN.rn(max, min));
+	    }
+	  }
 	
 }
