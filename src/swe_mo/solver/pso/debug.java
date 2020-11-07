544bb51414aa4893226c94193572791fe8572326
jonas1210 <jonas.dueckmann@gmail.com>
2020-11-07 12:01:05 +0100

    psoParticle update

--- src/swe_mo/solver/pso/debug.java
+++ src/swe_mo/solver/pso/debug.java
@@ -4,5 +4,6 @@ public class debug {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
 	}
 }
--- src/swe_mo/solver/pso/psoParticle.java
+++ src/swe_mo/solver/pso/psoParticle.java
@@ -12,29 +12,44 @@ public class psoParticle extends Particle_DE {
 	ArrayList<Double> velocity = new ArrayList<Double>();
 	ArrayList<Double> personalBestPosition = new ArrayList<Double>();
 	double personalMinimum;
-	double w, cc, cs;	
+	double w, cc, cs;  								//wie werden sie inizialisiert? setter/getter-Methoden?	
 	
 	
 	public psoParticle(int dimension, double max, double min) {
 			//This constructor creates a particle with the given dimension 
 			//and initializes all dimensions with a random number within the given bounds
 		super(dimension, max, min);
-		UpdateVelocity();
+		initializesVelocity();
+		initializesPersonalBestPosition();
 	}
 		
 	
 	public psoParticle(int dimension) {
 		//This constructor creates a particle with the given dimension and initializes all dimensions with zero
 		super(dimension);
-		UpdateVelocity();
+		initializesVelocity();
+		initializesPersonalBestPosition();
 	}
 	
 	
-	public void UpdateVelocity() {
-		if(velocity.isEmpty()) {		//Initializes first velocity.
-			this.velocity=this.position;
-		}else {							//Velocity update
-			
+	public void initializesVelocity(){
+		for(int i=0;i<this.position.size();i++) {
+			this.velocity.add(this.position.get(i));
+		}
+		
+	}
+	
+	public void initializesPersonalBestPosition() {
+		for(int i=0;i<this.position.size();i++) {
+			this.personalBestPosition.add(this.position.get(i));
+		}
+	}
+	
+	public void updateVelocity(psoParticle globalBestPosition) {
+		double rc=Math.random();
+		double rs=Math.random();
+		for(int i=0; i<this.velocity.size(); i++) {
+			this.velocity.set(i, w*this.velocity.get(i)  +  cc*rc*(this.personalBestPosition.get(i)-this.position.get(i))  +  cs*rs*(globalBestPosition.position.get(i)-this.position.get(i)));
 		}
 	}
 		
@@ -42,4 +57,12 @@ public class psoParticle extends Particle_DE {
 	public void updatePersonalBestPosition() {
 		this.personalBestPosition=this.position;
 	}
+	
+	
+	public void updatePosition() {
+		for(int i=0; i<this.position.size(); i++) {
+			this.position.set(i, this.position.get(i)+this.velocity.get(i));
+		}
+	}
+	
 }
