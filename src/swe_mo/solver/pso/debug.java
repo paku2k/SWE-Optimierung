89325f1516004da81de4f051bb5dc64c85d18cbd
eliasniepoetter <eliasniepoetter@gmail.com>
2020-11-08 16:25:57 +0100

    psoGlobal V0

--- src/swe_mo/solver/pso/debug.java
+++ src/swe_mo/solver/pso/debug.java
@@ -3,7 +3,10 @@ package swe_mo.solver.pso;
 public class debug {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World, again!");
+		
+		
+		
+		
 		
 	}
 	
--- src/swe_mo/solver/pso/psoGlobal.java
+++ src/swe_mo/solver/pso/psoGlobal.java
@@ -1,5 +1,35 @@
 package swe_mo.solver.pso;
 
+import java.util.ArrayList; 
+
 public class psoGlobal {
 
+	int dimension;
+	double globalMinimum;
+	int particleCount;
+	
+	ArrayList<Double> constraints = new ArrayList<Double>();
+	double [] globalBestPosition;
+	
+		public psoGlobal(int dimension, ArrayList<Double> constraints, int particleCount) {
+		
+			this.dimension = dimension;
+			this.constraints = constraints;
+			this.particleCount = particleCount;
+		}
+	
+			public void solve() {
+				
+				ArrayList<psoParticle> swarm = new ArrayList<psoParticle>(particleCount);
+				for(int i=0; i<particleCount; i++) {
+					// swarm.get(i).initialize(constraints);
+					
+				}
+			}
+			
+			public void updateGlobalBestPosition() {
+				
+				
+			}
+			
 }
