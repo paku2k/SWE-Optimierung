d76fb54bbf8c2a74bf7f78bdb16745edef8d4745
paku2k <paul.kleeblatt@gmail.com>
2020-11-13 17:49:21 +0100

    Integrated DE into GUI

--- src/swe_mo/solver/SolverResult.java
+++ src/swe_mo/solver/SolverResult.java
@@ -1,22 +1,36 @@
 package swe_mo.solver;
+import swe_mo.solver.de.Particle_DE;
+
 
 public class SolverResult {
-	double value;
-	//particle
+	public double value;
+	public Particle_DE particle;
+	public int ffCounter;
 	//other messages (number of iterations until result, ...)
 	
 	Exception e; //bitte stehen lassen, hier speichere ich die Exception, falls ihr in der solve Methode eine werft
 	
+	
+	public SolverResult() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public SolverResult(double value, Particle_DE particle, int ffCounter) {
+		this.value = value;
+		this.particle = particle;
+		this.ffCounter=ffCounter;
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 	
 
 	@Override
 	public String toString() {
-		return ""+value;
+		return "Value: "+value+"\n Particle: "+particle.toString()+"\nFitness functions run: "+ffCounter;
 	}
 	
 	public String toJSON() {
-		return "{\"value\":"+value+"}";
+		return "{\"value\":"+value+",\"ffCounter\":"+ffCounter+" ,\"particle\": "+particle.toString()+"}";
 	}
 }
