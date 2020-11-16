6b047428aed0a32d854a8f42fc07986a7b93448b
eliasniepoetter <eliasniepoetter@gmail.com>
2020-11-17 00:34:49 +0100

    update opti

--- src/swe_mo/solver/pso/Debug.java
+++ src/swe_mo/solver/pso/Debug.java
@@ -14,34 +14,34 @@ public class Debug {
 		int dimension = 30;
 		double min = -500;
 		double max = 500;
-		int particleCount = 500;
+		int particleCount = 2000;
 		double w = 0.9;
-		double cc = 0.9;
-		double cs = 0.1;
-		double dt = 1;
-		int numIter = 1000;
+		double cc = 0.83;
+		double cs = 0.022;
+		double dt = 1.9;
+		int numIter = 5000;
 		int solverID = 1;
 		int ffID = 8;
 		double decayStart = 0.9;
 		double decayEnd = 0.4;
 		
-		//PSOgscDecay psoGlobalSolver = new PSOgscDecay(dimension, min, max, particleCount, w, cc, cs, dt, numIter, ffID, solverID, decayStart, decayEnd);
+		PSOgscDecay psoGlobalSolver = new PSOgscDecay(dimension, min, max, particleCount, w, cc, cs, dt, numIter, ffID, solverID, decayStart, decayEnd);
 		SolverResult sr = new PSOgsc(dimension, min, max, particleCount, w, cc, cs, dt, numIter, ffID, solverID).solve();
 
 		System.out.println(sr.toString());
 		
 		// Run the Optimizer here:
 		double w_upper = 0.9;
-		double w_lower = 0.1;
-		double cc_upper = 0.9;
-		double cc_lower = 0.1;
-		double cs_upper = 0.9;
-		double cs_lower = 0.1;
-		int resolution = 10;
-		String filename = new String("psoOptimizerTest1");
+		double w_lower = 0.9;
+		double cc_upper = 0.85;
+		double cc_lower = 0.75;
+		double cs_upper = 0.03;
+		double cs_lower = 0.015;
+		int resolution = 20;
+		String filename = new String("f8_N30_pc1000_numIter10000_dt1,9");
 
-		PSOoptimizer testOpti = new PSOoptimizer(filename, dimension, min, max, particleCount, dt, numIter, ffID, solverID);
-		testOpti.psoGlobalLinear3DimensionalGridSearch(resolution, w_upper, w_lower, cc_upper, cc_lower, cs_upper, cs_lower);
+		//PSOoptimizer testOpti = new PSOoptimizer(filename, dimension, min, max, particleCount, dt, numIter, ffID, solverID);
+		//testOpti.psoGlobalLinear3DimensionalGridSearch(resolution, w_upper, w_lower, cc_upper, cc_lower, cs_upper, cs_lower);
 		
 	}
 }
