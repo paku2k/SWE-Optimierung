11c483e27f4f65229432b237723c2f2e4fb3f8aa
jonas1210 <jonas.dueckmann@gmail.com>
2020-11-23 13:30:19 +0100

    Parameteränderung in der pso-debug-klasse

--- src/swe_mo/solver/pso/PSOnsc.java
+++ src/swe_mo/solver/pso/PSOnsc.java
@@ -1,7 +1,9 @@
 package swe_mo.solver.pso;
 
-public class PSOnsc {
+public class PSOnsc extends PSOgsc {
 
-	
+	public PSOnsc(int dimension, double min, double max, int particleCount, double w, double cc, double cs, double dt, int numIter,  int ffID, int solverID) {
+		super(dimension, min, max, particleCount, w, cc, cs, dt, numIter,  ffID, solverID);
+	}
 	
 }
