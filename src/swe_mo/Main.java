e4941ffd13a56be767a788db1325949284fe635c
davidmessow <github@david-messow.de>
2020-12-21 11:30:57 +0100

    RELEASE V0.4 (with updated mov and exe)

--- src/swe_mo/Main.java
+++ src/swe_mo/Main.java
@@ -5,9 +5,9 @@ import swe_mo.ui.*;
 
 
 public class Main {	
-	public final static String APPVERSION = "0.0.3.9";
+	public final static String APPVERSION = "0.4.0.0";
 	public final static String DEVELOPERS = "Jonas Dückmann\nDaniel Bätjer\nElias Niepötter\nSimon Pauka\nDavid Messow";
-	public final static String DATE = "2020-12-19";
+	public final static String DATE = "2020-12-21";
 	
 	public static void main(String[] args) {
 		
